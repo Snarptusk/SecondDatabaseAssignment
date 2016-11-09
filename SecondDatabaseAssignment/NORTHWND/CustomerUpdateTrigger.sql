15ee34749549b3474107a6644e25c6724d5ddbba
Linus Viksten <linus94w@hotmail.com>
2016-11-09 13:02:26 +0100

    Final commit

--- SecondDatabaseAssignment/NORTHWND/CustomerUpdateTrigger.sql
+++ SecondDatabaseAssignment/NORTHWND/CustomerUpdateTrigger.sql
@@ -1,19 +1,4 @@
-﻿CREATE TABLE [dbo].[CustomerContactNameChanges](
-    [ContactNameChangeID] [int] IDENTITY(1,1) NOT NULL,
-    [ContactID] [int] NOT NULL,
-    [OldName] [text] NOT NULL,
-    [NewName] [text] NOT NULL,
-    [ChangedDate] [datetime] NOT NULL,
-    [UserId] [int] NOT NULL,
-    PRIMARY KEY CLUSTERED 
-    (
-        [ContactNameChangeID] ASC
-    )
-)
-
-GO
-
-CREATE TRIGGER [CustomerUpdateTrigger]
+﻿CREATE TRIGGER [CustomerUpdateTrigger]
 ON [dbo].[Customers]
 FOR UPDATE
 
@@ -27,6 +12,6 @@ BEGIN
 
 	SELECT i.[CustomerID], d.[ContactName], i.[ContactName], GETDATE(), USER_ID()
             FROM inserted i
-			INNER JOIN deleted d ON i.[CustomerID]=d.[CustomerID]
+              INNER JOIN deleted d ON i.[CustomerID]=d.[CustomerID]
             WHERE d.[ContactName] <> i.[ContactName]
 END
---
+++ SecondDatabaseAssignment/NORTHWND/dbo/Tables/CustomerNameChanged.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[CustomerContactNameChanges](
+    [ContactNameChangeID] [int] IDENTITY(1,1) NOT NULL,
+    [ContactID] TEXT NOT NULL,
+    [OldName] [text] NOT NULL,
+    [NewName] [text] NOT NULL,
+    [ChangedDate] [datetime] NOT NULL,
+    [UserId] [int] NOT NULL,
+    PRIMARY KEY CLUSTERED 
+    (
+        [ContactNameChangeID] ASC
+    )
+)
+
